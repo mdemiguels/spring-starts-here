5102d66e03216710470451ff6428d4b44754eaac
mdemiguels <miguelschz1997@gmail.com>
2024-09-21 22:20:55 +0200

    second method for access to spring context (@Component)

---
+++ spring_context/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,20 @@
+package config;
+
+import beans.Parrot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("beans") // Method 2 to add Beans to the Spring Context
+public class ProjectConfig {
+
+//    Method 1 to add Beans to the Spring Context
+//    @Bean
+//    public Parrot parrot() {
+//        Parrot parrot = new Parrot();
+//        parrot.setName("Kesha");
+//        return parrot;
+//    }
+
+}
---
+++ spring_context/src/main/java/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import beans.Parrot;
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Parrot p = context.getBean(Parrot.class);
+
+        System.out.println(p.getName());
+    }
+
+}
