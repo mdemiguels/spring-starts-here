ed1235e42f2b02adfa100c04b3dae00aa12df4a6
mdemiguels <miguelschz1997@gmail.com>
2025-02-23 19:42:23 +0100

    receiving JSON data from the client with @RequestBody

--- rest-services/src/main/java/com/mdemiguels/restservices/controller/PaymentController.java
+++ rest-services/src/main/java/com/mdemiguels/restservices/controller/PaymentController.java
@@ -1,27 +1,31 @@
 package com.mdemiguels.restservices.controller;
 
-import com.mdemiguels.restservices.exceptions.NotEnoughMoneyException;
-import com.mdemiguels.restservices.service.ErrorDetails;
 import com.mdemiguels.restservices.service.PaymentDetails;
 import com.mdemiguels.restservices.service.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
 
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
     private final PaymentService paymentService;
+
 
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-
-        PaymentDetails paymentDetails = paymentService.processPayment();
+    public ResponseEntity<PaymentDetails> makePayment(
+            @RequestBody PaymentDetails paymentDetails) {
+        
+        logger.info("Received payment " + paymentDetails.getAmount());
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body(paymentDetails);
