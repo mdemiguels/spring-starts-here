dca238f0dd2757d3f1e432a4f9bd1cc4d4b45278
mdemiguels <miguelschz1997@gmail.com>
2025-03-14 15:36:37 +0100

    spring boot transactions: create a transfer money service that uses transactions to keep data consistent

---
+++ spring-transactions/src/test/java/com/mdemiguels/spring_transactions/SpringTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdemiguels.spring_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
